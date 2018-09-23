27d165bdc5a695ca6e218cb6f7db84f7c55a44d3
byt3_h34d <mr.tybopp@gmail.com>
2018-09-22 19:26:17 -0500

    Fixing API Route Handling

--- app/public/assets/js/API-event-handling.js
+++ app/public/assets/js/API-event-handling.js
@@ -16,10 +16,10 @@ $("#search-btn").on("click", function () {
   searchedCity = searchedCity.replace(/\s+/g, "").toLowerCase();
 
   // Splits search variable at the comma, seporating city and state into an array of two seperate strings.
-  // searchedCity = searchedCity.split(",");
+  searchedCity = searchedCity.split(",");
 
   // Re-defines search variable as single string, inserting a character '&' in between city and state (because we cannot pass a comma in the AJAX URL for our API).
-  // searchedCity = searchedCity[0] + "&" + searchedCity[1];
+  searchedCity = searchedCity[0] + "&" + searchedCity[1];
 
   // run an AJAX GET-request for our servers api,
   // including the user's search criterion in the url
@@ -32,21 +32,13 @@ $("#search-btn").on("click", function () {
     if (!data) {
       $("#test-data-dump").append("<h2> Hmmm... No data was returned from database. Try another city. </h2>");
     } else {
-//<<<<<<< tybopp
       // otherwise, append the search result data to the test-data-dump div at the bottom of the page:
-      //$("#test-data-dump").append("<h2>" + data.Areaname + "</h2>");
+      $("#test-data-dump").append("<h2>" + data.Areaname + "</h2>");
       // Example Data Set #1
-      //$("#test-data-dump").append("<h3>Data Set #1: " + data.STCOU + "</h3>");
+      $("#test-data-dump").append("<h3>Data Set #1: " + data.STCOU + "</h3>");
       // Example Data Set #2
-      //$("#test-data-dump").append("<h3>Data Set #2: " + data.PST100209D + "</h3>");
-//=======
-      // otherwise
-      // append the character name
-      $("#test-data-dump").append("<h2> Areanam:"+ data.areaname + "</h2>");
-      // the role
-      $("#test-data-dump").append("<h3>Data Set #1: " + data.stateCounty + "</h3>");
-      // the age
-      $("#test-data-dump").append("<h3>Data Set #2: " + data.totalPopulation + "</h3>");
+      $("#test-data-dump").append("<h3>Data Set #2: " + data.PST100209D + "</h3>");
+
 
       // General side bar graph (blue)
       $("#percentPopulationOver65").append(`<span class="bar-fill" style="width:${data.populationOver65}%;"></span>`);
@@ -63,7 +55,6 @@ $("#search-btn").on("click", function () {
       $("#demVote2004").append(`<div class="bar bar-12 stat-3" style="height:${data.percentageDem2004}%;"></div>`);
       $("#repVote2008").append(`<div class="bar bar-13 stat-1" style="height:${data.percentageRep2008}%;"></div>`);
       $("#demVote2008").append(`<div class="bar bar-15 stat-3" style="height:${data.percentageDem2008}%;"></div>`);
-//>>>>>>> master
     }
   });
 });
